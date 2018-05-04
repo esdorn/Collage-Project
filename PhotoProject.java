bfcdbddece1d2a8c32ccdf9d51536e809fe0004e
Evan Dorn <esdorn@stu.naperville203.org>
2018-05-04 07:59:22 -0500

    5/4

--- PhotoProject.java
+++ PhotoProject.java
@@ -2,8 +2,8 @@
 
 /**
  * Write a description of class PhotoProject here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 import java.awt.*;
@@ -17,10 +17,10 @@ public class PhotoProject
         Picture myPic2 = new Picture("images/MH.jpg");
         Picture canvas2 = new Picture("images/MyCanvas.jpg");
         Picture scale = new Picture("images/Scale.jpg");
-        
+
         //First photo
         //myPic.copy(canvas2,0,0);
-        
+
         //Second photo
         /*
         myPic.qSwap();
@@ -30,7 +30,7 @@ public class PhotoProject
         scale.copy(canvas2,1920,540);
         scale.copy(canvas2,2880,540);
         */
-        
+
         //Third photo
         /*
         myPic.grey();
@@ -38,20 +38,20 @@ public class PhotoProject
         myPic.blend(myPic2);
         myPic.copy(canvas2,3840,0);
         */
-        
+
         //Forth photo
         //myPic.flip();
         //myPic.copy(canvas2,0,1080);
-        
+
         //Fifth photo
         //myPic.DMirror();
         //myPic.copy(canvas2,1920,1080);
-        
+
         //Sixth photo
-        myPic.Bgrey();
-        myPic.copy(canvas2,3840,1080);
-        
+        //myPic.Bgrey();
+        //myPic.copy(canvas2,3840,1080);
+
         //Save line
-        canvas2.write("images/MyCanvas.jpg");
+        //canvas2.write("images/MyCanvas.jpg");
     }
 }
